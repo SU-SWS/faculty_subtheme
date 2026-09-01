13ea7a6f03e00e22accd07536f6588d9df3eb0cf
Mike Decker <pookmish@stanford.edu>
2026-09-01 11:13:00 -0700

    Refactor footer and brand bar

---
+++ src/Hook/ThemeHooks.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\faculty_subtheme\Hook;
+
+use Drupal\Core\Extension\ThemeExtensionList;
+use Drupal\Core\Hook\Attribute\Hook;
+
+class ThemeHooks {
+
+  public function __construct(protected ThemeExtensionList $themeExtensionList) {}
+
+  /**
+   * Prepares variables for the html.html.twig template.
+   */
+  #[Hook('preprocess_html')]
+  public function preprocessHtml(&$variables): void {
+    $variables['stanford_basic_path'] = $this->themeExtensionList->getPath('stanford_basic');
+  }
+
+  /**
+   * Hide lockup wordmark.
+   */
+  #[Hook('preprocess_config_pages__stanford_local_footer')]
+  function preprocessConfigPagesStanfordLocalFooter(&$variables) {
+    $variables['lockup_title'] = FALSE;
+  }
+
+  /**
+   * Implements hook_theme_suggestions_HOOK_alter().
+   */
+  #[Hook('theme_suggestions_block_alter')]
+  public function themeSuggestionsBlockAlter(array &$suggestions, array $variables): void {
+    if (!empty($variables['elements']['#id']) && $variables['elements']['#id'] == 'faculty_subtheme_search') {
+      $suggestions[] = 'block__stanford_basic_search';
+    }
+  }
+
+}
